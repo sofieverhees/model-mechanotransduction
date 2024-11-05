37e233bea7d299a45205d21cef2d41c0b9bbceef
Sofie Verhees <91689345+sofieverhees@users.noreply.github.com>
2024-11-05 12:03:30 +0000

    Update loop.sh

--- signalling_model/loop.sh
+++ signalling_model/loop.sh
@@ -1,22 +1,17 @@
 #!/bin/bash
 # run different versions of the coupled model
 
-for mesh_name in 'cell_substrate'
+T = 10
+dt = 0.05
+mesh_name = 'cell_substrate'
+
+for D1 in 40 100 #40 100
 do
-    for T in 10
+    for E in 0.001 0.01 0.1 0.3 1 5.7 10 50 100 7000000 #0.001 0.01 0.1 0.3 1 5.7 10 50 100 7000000
     do
-        for dt in 0.05
-        do
-            for D1 in 40 100 #40 100
-            do
-                for E in 0.001 0.01 0.1 0.3 1 5.7 10 50 100 7000000 #0.001 0.01 0.1 0.3 1 5.7 10 50 100 7000000
-                do
-                    for twoDstim in '2D' '2xD' '3D' #'2D' '2xD' '3D' 
-                    do 
-                        python rho_model_reduced_wFAK.py -T $T -dt $dt -E $E -D1 $D1 -mesh_name $mesh_name -twoDstim $twoDstim
-                    done
-                done
-            done
+        for twoDstim in '2D' '2xD' '3D' #'2D' '2xD' '3D' 
+        do 
+            python main_signalling.py -T $T -dt $dt -E $E -D1 $D1 -mesh_name $mesh_name -twoDstim $twoDstim
         done
     done
 done
